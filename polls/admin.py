797990aaccb2f9d9434bde637a08cb337a7c07ca
zhangliutian <521348888@qq.com>
2018-01-04 03:31:33 +0800

    done

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from.models import Question
 
-admin.site.register(Question)
+from.models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['question_text'] # 添加一个搜索框
+    list_filter = ['pub_date'] # 添加一个过滤器
+    list_display = ('question_text', 'pub_date', 'was_published_recently') # 更改显示问题的列表页
+    fieldsets = [
+        (None,              {'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
